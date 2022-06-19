7f1e37830dea348289e0acfc0b8ed6c845d2b908
autroshot <autroshot@gamil.com>
2022-06-19 11:12:24 +0900

    첫 커밋

---
+++ main.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const _url = req.url;
+  const queryData = url.parse(_url, true).query;
+  const pathName = url.parse(_url, true).pathname;
+
+  if (pathName === '/') {
+    if (queryData.id === undefined) {
+     
+
+      fs.readFile(`./data/${queryData.id || 'Index'}`, 'utf-8', (err, description) => {
+        let title = 'Welcome';
+        description = 'Hello, Node.js';
+        if (err) throw err;
+        
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+    
+        res.statusCode = 200;
+        res.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
+        if (err) throw err;
+
+        let title = queryData.id;
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+    
+        res.statusCode = 200;
+        res.end(template);
+      });
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

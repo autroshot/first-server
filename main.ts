258fe39ef2d67ea1257aa4ed2f02790d71346218
autroshot <autroshot@gamil.com>
2022-06-20 07:21:06 +0900

    리팩터링: createHtml 함수 추출

--- main.ts
+++ main.ts
@@ -20,31 +20,14 @@ const server = http.createServer(async (request, response) => {
       description = 'Hello, Node.js';
     } else {
       title = searchParams.get('id') as string;
-
       description = await readFile(`./data/${searchParams.get('id')}`, 'utf-8');
     }
 
     const files = await readdir('./data/')
     const ul = createUlElement(files);
-    
-    const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${ul}
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-    `;
 
     response.statusCode = 200;
-    response.end(template);
+    response.end(createHtml(title, ul, description));
   } else {
     response.statusCode = 404;
     response.end('Not found');
@@ -62,6 +45,28 @@ function createUlElement(arr: string[]): string {
     return previousValue + `<li><a href="/?id=${currentValue}">${currentValue}</a></il>`;
   }, '');
   result += `<ul>${lis}</ul>`;
+
+  return result;
+}
+
+function createHtml(title: string, lists: string, description: string): string {
+  let result = '';
+  
+  result += `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${lists}
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+  `;
 
   return result;
 }

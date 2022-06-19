b450265b5ff7ea5970caab47090b2716e693b8cb
autroshot <autroshot@gamil.com>
2022-06-19 13:45:51 +0900

    파일, 디렉터리 읽기 비동기화 처리

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const { resolve } = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const _url = req.url;
   const queryData = url.parse(_url, true).query;
   const pathName = url.parse(_url, true).pathname;
 
   if (pathName === '/') {
+    let title;
+    let description;
+
     if (queryData.id === undefined) {
+      title = 'Welcome';
+      description = 'Hello, Node.js';
+    } else {
+      title = queryData.id;
+
+      await new Promise((resolve) => {fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, data) => {
+        if (err) throw err;
+        
+        description = data;
+
+        resolve();
+      });})
+    }
+
+    await new Promise((resolve) => {
       fs.readdir('./data/', (err, files) => {
         if (err) throw err;
-
-        const title = 'Welcome';
-        const description = 'Hello, Node.js';
-
+  
         const lis = files.reduce((previousValue, currentValue) => {
           return previousValue + `<li><a href="/?id=${currentValue}">${currentValue}</a></il>`;
         }, '');
@@ -41,38 +57,12 @@ const server = http.createServer((req, res) => {
     
         res.statusCode = 200;
         res.end(template);
+
+        resolve();
       });
-
-      
-    } else {
-      fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
-        if (err) throw err;
-
-        let title = queryData.id;
-        const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-        `;
+    });
     
-        res.statusCode = 200;
-        res.end(template);
-      });
-    }
+    
   } else {
     res.statusCode = 404;
     res.end('Not found');

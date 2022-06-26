bff40740f155e1e81d3b7c8582888fec46298396
autroshot <autroshot@gamil.com>
2022-06-26 19:16:35 +0900

    SQL Prepared Statement로 변경

--- server/models/topic.ts
+++ server/models/topic.ts
@@ -8,7 +8,7 @@ export async function getAllTopics() {
 }
 
 export async function getTopicById(id: number) {
-  const [rows] = await pool.query<Topic[]>(`SELECT * FROM topic WHERE topic_id = ${id}`);
+  const [rows] = await pool.execute<Topic[]>('SELECT * FROM topic WHERE topic_id = ?', [id]);
   
   return rows[0];
 }
